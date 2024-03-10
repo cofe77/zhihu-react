9bb0760958f15a7d9743986fd5b80b3a4387863e
elosnoC <1004460703@qq.com>
2024-03-10 14:47:29 +0800

    🐞 fix(package.json): fix nodejs

--- src/components/HomeContentUnit/index.tsx
+++ src/components/HomeContentUnit/index.tsx
@@ -1,22 +1,42 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 
-import QAndAContent from "./QAndAContent"
+import QAndAContent from "./QAndAContent";
 
-import "./style.less"
-import AnswerOperate from "./AnswerOperate"
+import "./style.less";
+import AnswerOperate from "./AnswerOperate";
 
 const HomeContentUnit = (props: { question: any }) => {
-    const question = props.question
-    const [isFold, setIsFold] = useState(true)
-    return (
-        <div className="bd-content-unit" data-agree-state="agreeState" data-is-fold="false">
-            <div className="content-title"><a href={"http://localhost:3000/#/question/" + question.id} target="_blank" rel="noopener noreferrer">{question.title}</a></div>
-            <div className="content-main">
-                <QAndAContent question={question} isFold={isFold} expandHandle={() => setIsFold(false)} />
-            </div>
-            <AnswerOperate isFold={isFold} answer={question.answer} setIsFold={() => setIsFold(true)} />
-        </div>
-    )
-}
+  const question = props.question;
+  const [isFold, setIsFold] = useState(true);
+  return (
+    <div
+      className="bd-content-unit"
+      data-agree-state="agreeState"
+      data-is-fold="false"
+    >
+      <div className="content-title">
+        <a
+          href={"/question/" + question.id}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {question.title}
+        </a>
+      </div>
+      <div className="content-main">
+        <QAndAContent
+          question={question}
+          isFold={isFold}
+          expandHandle={() => setIsFold(false)}
+        />
+      </div>
+      <AnswerOperate
+        isFold={isFold}
+        answer={question.answer}
+        setIsFold={() => setIsFold(true)}
+      />
+    </div>
+  );
+};
 
-export default HomeContentUnit
+export default HomeContentUnit;
